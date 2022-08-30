a6452a3ebd7a2d6a44e189c2c8d8aeace408bafa
Saro Henyo <cryptohenyo@gmail.com>
2022-08-30 21:04:06 +0800

    Bravo commit

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,18 +2,31 @@ import React from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
 import { selectRoomId } from "../redux/features/appSlices";
-import { useDocument } from "react-firebase-hooks/firestore";
+import { useCollection, useDocument } from "react-firebase-hooks/firestore";
 import { db } from "../firebase";
+import ChatInput from "./ChatInput";
+import Message from "./Message";
 
 // Icons
 import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+
 
 export default function Chat() {
   const roomId = useSelector(selectRoomId);
   const [roomDetails] = useDocument(
     roomId && db.collection("rooms").doc(roomId)
   );
+
+  const [roomMessages, loading] = useCollection(
+    roomId &&
+      db
+        .collection("rooms")
+        .doc(roomId)
+        .collection("messages")
+        .orderBy("timestamp", "asc")
+  );
+
   return (
     <ChatContainer>
       <>
@@ -31,6 +44,24 @@ export default function Chat() {
             </p>
           </HeaderRight>
         </Header>
+
+        <ChatMessages>
+          {roomMessages?.docs.map((doc) => {
+            const { message, timestamp, user, userImage } = doc.data();
+            return (
+              <Message
+                key={doc.id}
+                message={message}
+                timestamp={timestamp}
+                user={user}
+                userImage={userImage}
+              />
+            );
+          })}
+        </ChatMessages>
+
+
+        <ChatInput channelName={roomDetails?.data().name} channelId={roomId} />
       </>
     </ChatContainer>
   );
@@ -78,3 +109,6 @@ const HeaderRight = styled.div`
     font-size: 16px;
   }
 `;
+
+
+const ChatMessages = styled.div``;
---
+++ src/components/Message.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from "styled-components"
+
+export default function Message({ userImage, user, timestamp }) {
+  return <MessageContainer>
+        <img src="{userImage}" alt="" />
+        <MessageInfo>
+                <h4>{user} <span>{new Date(timestamp?.toDate()).toUTCString()}</span></h4>
+        </MessageInfo>
+  </MessageContainer>;
+}
+
+
+const MessageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+
+  > img {
+    height: 50px;
+    border-radius: 8px;
+  }
+`;
+
+
+const MessageInfo = styled.div`
+  padding-left: 10px;
+
+  > h4 > span {
+    color: gray;
+    font-weight: 300;
+    margin-left: 4px;
+    font-size: 10px;
+  }
+`;
